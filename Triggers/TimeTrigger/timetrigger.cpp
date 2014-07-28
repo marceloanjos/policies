4fd72a6886c85acf56cfb7d025be09b36dfe8621
Pluginbot <bcairns@pluginbot.net>
2014-07-28 14:10:54 -0400

    Updated the time trigger

--- Triggers/TimeTrigger/timetrigger.cpp
+++ Triggers/TimeTrigger/timetrigger.cpp
@@ -63,12 +63,21 @@ bool TimeTrigger::triggered(QMap<QString,QVariant> params)
 {
     try
     {
+        qDebug() << " - checking trigger: " + name();
+
+        foreach(QString key, params.keys())
+        {
+            qDebug() << " - key: " + key + " = " + params.value(key).toString();
+        }
+
         //Make sure we have the required parameters
         if(!params.keys().contains("time")) return false;
 
         //Get the parameter
         QTime time = params.value("time").toTime();
         QTime current = QTime::currentTime();
+
+        qDebug() << " - time " << current.toString("hh:mm:ss");
 
         //See if the hour and minutes match
         if(current.hour() == time.hour() && current.minute() == time.minute()) return true;
